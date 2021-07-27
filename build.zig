12059333bd51a1c3790a7e61798b0c7c86cbc174
rvcas <x@rvcas.dev>
2021-07-27 17:12:55 -0400

    feat: add package

--- build.zig
+++ build.zig
@@ -3,7 +3,14 @@ const Builder = @import("std").build.Builder;
 pub fn build(b: *Builder) void {
     const mode = b.standardReleaseOptions();
     const exe = b.addExecutable("crisp", "src/main.zig");
+
     exe.setBuildMode(mode);
+
+    exe.addPackage(.{
+        .name = "zig-terminal",
+        .path = "deps/zig-terminal/src/main.zig",
+    });
+
     exe.install();
 
     const run_cmd = exe.run();
